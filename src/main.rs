50ab2a59355cd0d17810af6b6670300b44b35ff7
mvn-rvn <67172473+mvn-rvn@users.noreply.github.com>
2021-11-04 16:04:34 +0000

    e

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+//create grid
 fn build_grid() -> [[String; 7]; 6]  {
   let grid: [[String; 7]; 6] = [
     ["-".to_string(), "-".to_string(), "-".to_string(), "-".to_string(), "-".to_string(), "-".to_string(), "-".to_string()],
@@ -10,6 +11,7 @@ fn build_grid() -> [[String; 7]; 6]  {
   grid
 }
 
+//print grid to console
 fn print_grid(grid: &[[String; 7]; 6]) {
   println!("\n0 1 2 3 4 5 6");
   for row in grid {
@@ -21,10 +23,16 @@ fn print_grid(grid: &[[String; 7]; 6]) {
   print!("----------");
 }
 
+//main
 fn main() {
+  //build grid and set turn
   let mut grid = build_grid();
   let mut turn = String::from("X");
+  
+  //main gameloop
   loop {
-    
+    //PLACEHOLDER: Testing grid printing
+    print_grid(&grid);
+    break;
   }
 }
